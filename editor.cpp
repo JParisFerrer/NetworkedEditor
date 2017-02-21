bfbc7fe93b81a99c55a8edf8d698a36637c92adf
Jordi Paris Ferrer <jparisferrer@gmail.com>
2017-02-21 00:47:06 -0600

    no idea how this happened, but suddenly the windows work - SUCCESS

--- editor.cpp
+++ editor.cpp
@@ -13,8 +13,20 @@ void setup()
     cbreak();       // set it up so we read a character at a time
     noecho();       // prevent the screen from showing typed in characters
 
-    mainWindow = newwin(LINES - 1, COLUMNS, 0, 0);
-    commandWindow = newwin(1, COLUMNS, LINES-1, 0);
+    wrefresh(stdscr);
+
+    int w, h;
+    getmaxyx(stdscr, h, w);
+
+    mainWindow = newwin(h - 1, w, 0, 0);
+    commandWindow = newwin(1, w, h-1, 0);
+
+    //wrefresh(stdscr);
+
+    wrefresh(mainWindow);
+    wrefresh(commandWindow);
+
+    //delwin(stdscr);
     
     keypad(mainWindow, TRUE); // we should handle the special characters for the typing window, but not the command window
 }
@@ -23,10 +35,26 @@ int main(int argc, char** argv)
 {
     setup();
 
-    while(1)
-    {
-        wrefresh(mainWindow);
-        wrefresh(commandWindow);
-    }
+    waddstr(mainWindow, "hello world!");
+    waddstr(commandWindow, "hello world!");
+    //waddstr(stdscr, "ey");
+    //wprintw(stdscr, "%p, %p", mainWindow, commandWindow);
+
+    /*
+    box(mainWindow, 0, 0);
+    box(commandWindow, 0, 0);
+    box(stdscr, 0, 0);
+    */
+
+    wrefresh(mainWindow);
+    wrefresh(commandWindow);
+    //wrefresh(stdscr);
+
+    //delwin(stdscr);
+    getch();
+    getch();
+    getch();
+
+    endwin();
 }
 
