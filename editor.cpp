1b6ec0c91a4dd90f1e1d63d5b752fb52c2d4e394
Jordi Paris Ferrer <jparisferrer@gmail.com>
2017-02-21 00:51:29 -0600

    removed remnants of the 'permute every line of code until it works' strat

--- editor.cpp
+++ editor.cpp
@@ -21,12 +21,10 @@ void setup()
     mainWindow = newwin(h - 1, w, 0, 0);
     commandWindow = newwin(1, w, h-1, 0);
 
-    //wrefresh(stdscr);
 
     wrefresh(mainWindow);
     wrefresh(commandWindow);
 
-    //delwin(stdscr);
     
     keypad(mainWindow, TRUE); // we should handle the special characters for the typing window, but not the command window
 }
@@ -37,22 +35,11 @@ int main(int argc, char** argv)
 
     waddstr(mainWindow, "hello world!");
     waddstr(commandWindow, "hello world!");
-    //waddstr(stdscr, "ey");
-    //wprintw(stdscr, "%p, %p", mainWindow, commandWindow);
 
-    /*
-    box(mainWindow, 0, 0);
-    box(commandWindow, 0, 0);
-    box(stdscr, 0, 0);
-    */
 
     wrefresh(mainWindow);
     wrefresh(commandWindow);
-    //wrefresh(stdscr);
 
-    //delwin(stdscr);
-    getch();
-    getch();
     getch();
 
     endwin();
