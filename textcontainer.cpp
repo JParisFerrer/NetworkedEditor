b9a8eb076626be892e11e52d976aa111e4c4c437
armantajback <tajback2@illinois.edu>
2017-03-03 18:11:03 -0600

    filled out skeletons

--- textcontainer.cpp
+++ textcontainer.cpp
@@ -9,38 +9,37 @@ TextContainer<T>::TextContainer(){
 /*Core Functions*/
 template <typename T>
 void TextContainer<T>::add(size_t line, size_t index, int input){
-
-
+    this->container.add(line,index,input);
 }
 
 template <typename T>
 void TextContainer<T>::remove(size_t line, size_t index){
-
+    this->container.remove(line,index);
 }
 
 template <typename T>
 void TextContainer<T>::writeToFile(string fileName){
-
+    this->container.writeToFile(fileName);
 }
 
 template <typename T>
 void TextContainer<T>::readFromFile(string fileName){
-
+    this->container.readFromFile(fileName);
 }
 
 template <typename T>
 void TextContainer<T>::print(size_t line,size_t maxWidth){
-
+    this->container.print(line, maxWidth);
 }
 
 /*debugging utilties*/
 
 template <typename T>
 void TextContainer<T>::printDebug(){
-
+    this->container.printDebug();
 }
 
 template <typename T>
 void TextContainer<T>::writeToFileDebug(){
-
+    this->container.writeToFileDebug();
 }
--- textcontainer.h
+++ textcontainer.h
@@ -5,7 +5,7 @@
 template <typename T>
 class TextContainer {
 public:
-  /*Constructors*/
+  /*Constructors*/=
     TextContainer();
 
   /*Core Functions*/
