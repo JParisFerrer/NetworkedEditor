508107f28f81cafde0827e843c6fd41088185857
Jordi Paris Ferrer <jparisferrer@gmail.com>
2017-04-23 21:15:19 -0500

    added string highlighting

--- blockingVector.cpp
+++ blockingVector.cpp
@@ -206,11 +206,12 @@ void BlockingVector::printColored(WINDOW* win, std::string text)
     init_pair(1, COLOR_YELLOW, COLOR_BLACK);
     init_pair(2, COLOR_CYAN, COLOR_BLACK);
     init_pair(3, COLOR_BLUE, COLOR_BLACK);
+    init_pair(4, COLOR_RED, COLOR_BLACK);
 
     std::vector<int> ctext(text.begin(), text.end());
 
     // loop over regex matches
-    std::vector<std::pair<std::string, int>> keywords = {std::make_pair("\\bfor\\b", 3), std::make_pair("\\bwhile\\b", 3), std::make_pair("\\bdo\\b", 3), std::make_pair("[\\d]+", 1)};
+    std::vector<std::pair<std::string, int>> keywords = {std::make_pair("\\bfor\\b", 3), std::make_pair("\\bwhile\\b", 3), std::make_pair("\\bdo\\b", 3), std::make_pair("[\\d]+", 1), std::make_pair("\"[^\"]*\"", 4)};
 
     std::string types[] = {"int", "long", "string", "char", "size_t", "ssize_t", "short", "bool", "[\\w]+_t"};
 
